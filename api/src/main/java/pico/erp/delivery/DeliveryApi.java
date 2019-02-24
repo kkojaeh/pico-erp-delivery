a3bbde08afa5ebc99d46da7e5b4647d20f047376
Ethan <kkojaeh@gmail.com>
2019-02-24 14:26:52 +0900

    권한 추가

--- api/src/main/java/pico/erp/delivery/DeliveryApi.java
+++ api/src/main/java/pico/erp/delivery/DeliveryApi.java
@@ -1,9 +1,25 @@
 package pico.erp.delivery;
 
+import javax.persistence.Id;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 import pico.erp.shared.ApplicationId;
+import pico.erp.shared.data.Role;
 
 public final class DeliveryApi {
 
   public static ApplicationId ID = ApplicationId.from("delivery");
+
+  @RequiredArgsConstructor
+  public enum Roles implements Role {
+
+    DELIVERY_ACCESSOR,
+    DELIVERY_CHARGER;
+
+    @Id
+    @Getter
+    private final String id = name();
+
+  }
 
 }
